b0db65f5786ae450f8fe702aa90ba93c8cd96b09
katzeX <xiusha.d@gmail.com>
2025-06-04 08:56:51 +0300

    add image and adjustments for DTO

--- inventory/src/main/java/com/uni/inventory/api/dto/request/VideoGameRequestDto.java
+++ inventory/src/main/java/com/uni/inventory/api/dto/request/VideoGameRequestDto.java
@@ -3,9 +3,23 @@ package com.uni.inventory.api.dto.request;
 import com.uni.inventory.model.enums.Platform;
 import com.uni.inventory.model.enums.RentalTier;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
 
-public record VideoGameRequestDto(
-        @NotNull(message = "Title shouldn't be null") String title,
-        @NotNull(message = "Rental tier shouldn't be null") RentalTier rentalTier,
-        @NotNull(message = "Platform shouldn't be null") Platform platform) {
+import java.util.Objects;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class VideoGameRequestDto {
+    private @NotNull(message = "Title shouldn't be null") String title;
+    private @NotNull(message = "Rental tier shouldn't be null") RentalTier rentalTier;
+    private @NotNull(message = "Platform shouldn't be null") Platform platform;
+    private @NotNull(message = "Image shouldn't be null") MultipartFile image;
 }
