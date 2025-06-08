3d4a85eebcda746c18c202f71acecacf0a33cabd
commin4u <commin4u.all@gmail.com>
2025-06-08 15:06:00 +0300

    Fix inventory tests: use H2 DB in test profile

--- inventory/src/test/java/com/uni/inventory/InventoryApplicationTests.java
+++ inventory/src/test/java/com/uni/inventory/InventoryApplicationTests.java
@@ -3,6 +3,7 @@ package com.uni.inventory;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class InventoryApplicationTests {
 
