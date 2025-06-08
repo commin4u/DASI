00dee8724efdc4dd9661b2dc3e7d64ca387b6fff
commin4u <commin4u.all@gmail.com>
2025-06-08 16:03:49 +0300

    Fixed test suite reliability and configuration for inventory and order services

--- order/src/test/java/com/uni/order/OrderApplicationTests.java
+++ order/src/test/java/com/uni/order/OrderApplicationTests.java
@@ -2,12 +2,13 @@ package com.uni.order;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class OrderApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
